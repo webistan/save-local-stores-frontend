9b21befa58da33055346b91b33edc543c3fea01d
Rouven Strauss <dev@rouvenstrauss.com>
2020-03-21 14:29:41 +0100

    Frontend: Add routes.

--- save-local-stores-frontend-code/src/app/app-routing.module.ts
+++ save-local-stores-frontend-code/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DetailViewPageComponent } from './detail-view-page/detail-view-page.component'
+import { LandingPageComponent } from './landing-page/landing-page.component'
+import { OverviewPageComponent } from './overview-page/overview-page.component'
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LandingPageComponent },
+  { path: 'overview', component: OverviewPageComponent },
+  { path: 'detail', component: DetailViewPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

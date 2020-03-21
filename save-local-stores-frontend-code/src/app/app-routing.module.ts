4151b5de85e60521483678e1ec0ad5c177c0adca
Bernhard Walter Freiherr von Rotenhan <werbemuell.wurst@gmail.com>
2020-03-21 16:01:32 +0100

    added routing

--- save-local-stores-frontend-code/src/app/app-routing.module.ts
+++ save-local-stores-frontend-code/src/app/app-routing.module.ts
@@ -3,11 +3,19 @@ import { Routes, RouterModule } from '@angular/router';
 import { DetailViewPageComponent } from './detail-view-page/detail-view-page.component'
 import { LandingPageComponent } from './landing-page/landing-page.component'
 import { OverviewPageComponent } from './overview-page/overview-page.component'
+import { ImpressumPageComponent } from './impressum-page/impressum-page.component'
+import { FaqPageComponent} from './faq-page/faq-page.component'
+import { DataPrivacyPageComponent} from './data-privacy-page/data-privacy-page.component'
+import { AgbPageComponent} from './agb-page/agb-page.component'
 
 const routes: Routes = [
   { path: '', component: LandingPageComponent },
   { path: 'overview', component: OverviewPageComponent },
-  { path: 'detail/:id', component: DetailViewPageComponent }
+  { path: 'detail/:id', component: DetailViewPageComponent },
+  { path: 'impressium', component: ImpressumPageComponent},
+  { path: 'faq', component: FaqPageComponent},
+  { path: 'datenschutz', component: DataPrivacyPageComponent},
+  { path: 'agb', component: AgbPageComponent}
 ];
 
 @NgModule({

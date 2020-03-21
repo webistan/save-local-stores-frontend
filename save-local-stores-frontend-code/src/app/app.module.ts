542f070ac717113a5122aec9e96e92dad2cc80f9
Rouven Strauss <dev@rouvenstrauss.com>
2020-03-21 12:35:11 +0100

    Frontend: Added bottom bar component.

--- save-local-stores-frontend-code/src/app/app.module.ts
+++ save-local-stores-frontend-code/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { OverviewPageComponent } from './overview-page/overview-page.component';
 import { DetailViewPageComponent } from './detail-view-page/detail-view-page.component';
 import { VoucherOrderPageComponent } from './voucher-order-page/voucher-order-page.component';
 import { VoucherConfirmationPageComponent } from './voucher-confirmation-page/voucher-confirmation-page.component';
+import { BottomBarComponent } from './bottom-bar/bottom-bar.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { VoucherConfirmationPageComponent } from './voucher-confirmation-page/vo
     OverviewPageComponent,
     DetailViewPageComponent,
     VoucherOrderPageComponent,
-    VoucherConfirmationPageComponent
+    VoucherConfirmationPageComponent,
+    BottomBarComponent
   ],
   imports: [
     BrowserModule,

45ec340790325187a85ffad8b4d7bd21e15aa973
Rouven Strauss <dev@rouvenstrauss.com>
2020-03-21 16:00:49 +0100

    Frontend: Update routes.

--- save-local-stores-frontend-code/src/app/app-routing.module.ts
+++ save-local-stores-frontend-code/src/app/app-routing.module.ts
@@ -1,13 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AgbPageComponent } from './agb-page/agb-page.component';
+import { DataPrivacyPageComponent } from './data-privacy-page/data-privacy-page.component'
 import { DetailViewPageComponent } from './detail-view-page/detail-view-page.component'
+import { FaqPageComponent } from './faq-page/faq-page.component';
+import { ImpressumPageComponent } from './impressum-page/impressum-page.component'
 import { LandingPageComponent } from './landing-page/landing-page.component'
 import { OverviewPageComponent } from './overview-page/overview-page.component'
 
 const routes: Routes = [
   { path: '', component: LandingPageComponent },
+  { path: 'data-privacy', component: DataPrivacyPageComponent },
+  { path: 'detail/:id', component: DetailViewPageComponent },
+  { path: 'detail/:id', component: DetailViewPageComponent },
+  { path: 'faq', component: FaqPageComponent },
+  { path: 'terms', component: AgbPageComponent },
+  { path: 'impressum', component: ImpressumPageComponent },
   { path: 'overview', component: OverviewPageComponent },
-  { path: 'detail/:id', component: DetailViewPageComponent }
 ];
 
 @NgModule({

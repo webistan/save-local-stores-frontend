04adfea056f715e76342e1b0d08734107bdd845a
Rouven Strauss <dev@rouvenstrauss.com>
2020-03-21 15:47:47 +0100

    app-routing: Update detail route with parameter.

--- save-local-stores-frontend-code/src/app/app-routing.module.ts
+++ save-local-stores-frontend-code/src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ import { OverviewPageComponent } from './overview-page/overview-page.component'
 const routes: Routes = [
   { path: '', component: LandingPageComponent },
   { path: 'overview', component: OverviewPageComponent },
-  { path: 'detail', component: DetailViewPageComponent }
+  { path: 'detail/:id', component: DetailViewPageComponent }
 ];
 
 @NgModule({

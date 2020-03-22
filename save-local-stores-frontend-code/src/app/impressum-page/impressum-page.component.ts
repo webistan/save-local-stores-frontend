88d795f92195735aa0f73db05e4e78591d39e86f
Rouven Strauss <dev@rouvenstrauss.com>
2020-03-22 08:49:35 +0100

    Impressum: Formatted text.

--- save-local-stores-frontend-code/src/app/impressum-page/impressum-page.component.ts
+++ save-local-stores-frontend-code/src/app/impressum-page/impressum-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { DOMAIN_NAME, WWW_DOMAIN_NAME } from '../model/global';
+import { BUSINESS_NAME, DOMAIN_NAME, WWW_DOMAIN_NAME } from '../model/global';
 
 @Component({
   selector: 'app-impressum-page',
@@ -8,10 +8,12 @@ import { DOMAIN_NAME, WWW_DOMAIN_NAME } from '../model/global';
   styleUrls: ['./impressum-page.component.css']
 })
 export class ImpressumPageComponent implements OnInit {
+  businessName: string
   domainName: string
   wwwDomainName: string
 
   constructor() {
+    this.businessName = BUSINESS_NAME
     this.domainName = DOMAIN_NAME
     this.wwwDomainName = WWW_DOMAIN_NAME
   }
--- save-local-stores-frontend-code/src/app/model/global.ts
+++ save-local-stores-frontend-code/src/app/model/global.ts
@@ -1,4 +1,5 @@
 export const APP_NAME = 'kiezmarie'
+export const BUSINESS_NAME = 'Kiezmarie GbR'
 export const DOMAIN_NAME = 'kiezmarie.de'
 export const WWW_DOMAIN_NAME = 'www.' + DOMAIN_NAME
 export const CITIES_SUBPATH = 'cities'
